8af4b59ef26eaf1370f4614e5a5247f39e402d42
duangsuse <fedora-opensuse@outlook.com>
2018-08-04 10:13:05 +0800

    Add UI for popup

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -12,6 +12,49 @@ let $$ = mdui.JQ;
 // + OTP Client
 // + Accounts list
 // + Go to options
-// + Autofill blacklist website
-// + QR share
-// + Manual share / import
+// + Autofill blacklist website (moved to options menu)
+// + QR share (moved to options menu)
+// + Manual share (moved to options menu) / import
+
+/**
+ * Make a excited chain call function
+ *
+ * @param {Function} fn function to 2nd chain call
+ * @param {Function} inner fuction to 1st chain call
+ */
+function chian(fn, inner) {
+    return ((o) => {
+        return fn(inner(o));
+    });
+}
+
+/**
+ * Make a mdui tip content object
+ *
+ * @param {String} m message
+ */
+function _C(m) {
+    return {
+        content: m
+    };
+}
+
+let _Tip = chian(_C, _M);
+
+// Add account manually
+function addAccount() {
+
+    return false;
+}
+
+// Setup tooltips
+new mdui.Tooltip('#setup', _Tip('Options'));
+new mdui.Tooltip('#add', _Tip('ManualImport'));
+
+// Setup events
+$$('#setup').on('click', () => {
+    browser.tabs.create({
+        url: browser.extension.getURL('options/options.html')
+    }); // Ignore errors
+});
+$$('#add').on('click', addAccount);
