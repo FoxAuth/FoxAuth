b78dd70db7f46ddbd38a88064b678dee99f23056
duangsuse <fedora-opensuse@outlook.com>
2018-08-04 11:07:18 +0800

    Add theme chager

--- src/options/options.js
+++ src/options/options.js
@@ -47,6 +47,7 @@ new mdui.Tooltip('#title', _Tip('Version'));
 new mdui.Tooltip('#export', _Tip('ImportExport'));
 new mdui.Tooltip('#backup', _Tip('Backup'));
 new mdui.Tooltip('#crypt', _Tip('Crypt'));
+new mdui.Tooltip('#theme', _Tip('Theme'));
 
 // Should use dark style from 19:00 to 6:00
 function shouldApplyDarkStyle() {
@@ -95,6 +96,12 @@ $$('#wi').text(_M('doImport'));
 $$('#we').text(_M('doExport'));
 $$('#qi').text(_M('doExportQR'));
 $$('#qe').text(_M('doExportText'));
+
+$$('#tsetup').text(_M('ThemeSetup'));
+$$('#ttheme').text(_M('Theme'));
+$$('#tprimary').text(_M('ThemePrimary'));
+$$('#taccent').text(_M('ThemeSecoundary'));
+$$('#tdefault').text(_M('RestoreDefault'));
 
 autodark();
 
@@ -178,3 +185,110 @@ _K('#ee', _S(encrypted, false));
 _K('#ac', blackOptions);
 _K('#wi', _S(backup, true));
 _K('#we', _S(backup, false));
+
+// MDUI code
+
+_K('#theme', () => new mdui.Dialog('#dialog-theme', {}).open());
+
+/**
+ * 设置文档主题
+ */
+(function () {
+    var DEFAULT_PRIMARY = 'indigo';
+    var DEFAULT_ACCENT = 'pink';
+    var DEFAULT_LAYOUT = '';
+
+    // 设置 cookie
+    var setCookie = function (key, value) {
+        // cookie 有效期为 1 年
+        var date = new Date();
+        date.setTime(date.getTime() + 365 * 24 * 3600 * 1000);
+        document.cookie = key + '=' + value + '; expires=' + date.toGMTString() + '; path=/';
+    };
+
+    var setDocsTheme = function (theme) {
+        if (typeof theme.primary === 'undefined') {
+            theme.primary = false;
+        }
+        if (typeof theme.accent === 'undefined') {
+            theme.accent = false;
+        }
+        if (typeof theme.layout === 'undefined') {
+            theme.layout = false;
+        }
+
+        var i, len;
+        var $body = $$('body');
+
+        var classStr = $body.attr('class');
+        var classs = classStr.split(' ');
+
+        // 设置主色
+        if (theme.primary !== false) {
+            for (i = 0, len = classs.length; i < len; i++) {
+                if (classs[i].indexOf('mdui-theme-primary-') === 0) {
+                    $body.removeClass(classs[i]);
+                }
+            }
+            $body.addClass('mdui-theme-primary-' + theme.primary);
+            setCookie('docs-theme-primary', theme.primary);
+            $$('input[name="doc-theme-primary"][value="' + theme.primary + '"]').prop('checked', true);
+        }
+
+        // 设置强调色
+        if (theme.accent !== false) {
+            for (i = 0, len = classs.length; i < len; i++) {
+                if (classs[i].indexOf('mdui-theme-accent-') === 0) {
+                    $body.removeClass(classs[i]);
+                }
+            }
+            $body.addClass('mdui-theme-accent-' + theme.accent);
+            setCookie('docs-theme-accent', theme.accent);
+            $$('input[name="doc-theme-accent"][value="' + theme.accent + '"]').prop('checked', true);
+        }
+
+        // 设置主题色
+        if (theme.layout !== false) {
+            for (i = 0, len = classs.length; i < len; i++) {
+                if (classs[i].indexOf('mdui-theme-layout-') === 0) {
+                    $body.removeClass(classs[i]);
+                }
+            }
+            if (theme.layout !== '') {
+                $body.addClass('mdui-theme-layout-' + theme.layout);
+            }
+            setCookie('docs-theme-layout', theme.layout);
+            $$('input[name="doc-theme-layout"][value="' + theme.layout + '"]').prop('checked', true);
+        }
+    };
+
+    // 切换主色
+    $$(document).on('change', 'input[name="doc-theme-primary"]', function () {
+        setDocsTheme({
+            primary: $$(this).val()
+        });
+    });
+
+    // 切换强调色
+    $$(document).on('change', 'input[name="doc-theme-accent"]', function () {
+        setDocsTheme({
+            accent: $$(this).val()
+        });
+    });
+
+    // 切换主题色
+    $$(document).on('change', 'input[name="doc-theme-layout"]', function () {
+        setDocsTheme({
+            layout: $$(this).val()
+        });
+    });
+
+    // 恢复默认主题
+    $$(document).on('cancel.mdui.dialog', '#dialog-docs-theme', function () {
+        setDocsTheme({
+            primary: DEFAULT_PRIMARY,
+            accent: DEFAULT_ACCENT,
+            layout: DEFAULT_LAYOUT
+        });
+    });
+})();
