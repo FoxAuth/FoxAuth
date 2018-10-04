476d14c815dbcc16371fe616a85b505bd40ceca2
Riajyuu <16912240+Riajyuu@users.noreply.github.com>
2018-10-04 20:56:41 +0800

    form dup function

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -5,8 +5,15 @@ function resetNotifi() {
         "iconUrl": "../icons/icon.svg",
         "title": "FoxAuth Authenticator",
         "message": "Reset done."
-      });
+    });
 };
 
 //genral option page reset
 document.querySelector('.resetbtn').addEventListener("click", resetNotifi);
+
+//create new OTP form
+document.querySelector('.otpNewBtn').addEventListener("click", () => {
+    const dom = document.querySelector('.newOTP');
+    const c = el.cloneNode(true);
+    dom.parentNode.appendChild(c);
+});
