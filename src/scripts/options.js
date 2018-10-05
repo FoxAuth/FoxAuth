9bf0cfa9deb5688454f8c1cf2eba822b3b7db7b8
Riajyuu <16912240+Riajyuu@users.noreply.github.com>
2018-10-05 23:09:21 +0800

    toggle secret token

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -35,3 +35,26 @@ document.getElementById("deleteOTP").addEventListener("click", () => {
       node.parentNode.removeChild(node);
     }
 });
+
+//toggle token
+function show() {
+    var p = document.getElementById('newSecretToken');
+    p.setAttribute('type', 'text');
+}
+
+function hide() {
+    var p = document.getElementById('newSecretToken');
+    p.setAttribute('type', 'password');
+}
+
+var pwShown = 0;
+
+document.getElementById("eye").addEventListener("click", function () {
+    if (pwShown == 0) {
+        pwShown = 1;
+        show();
+    } else {
+        pwShown = 0;
+        hide();
+    }
+}, false);
