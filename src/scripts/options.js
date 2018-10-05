aef51feb0f702620cbc90e2bef9d7b6f7a029fec
Riajyuu <16912240+Riajyuu@users.noreply.github.com>
2018-10-05 16:20:30 +0800

    toggle additional form items

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -11,6 +11,16 @@ function resetNotifi() {
 //genral option page reset
 document.querySelector('.resetbtn').addEventListener("click", resetNotifi);
 
+//toggle additional form items
+document.getElementById('moreFormbtn').addEventListener("click", () => {
+    var x = document.getElementById("moreFormItem");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+});
+
 //create new OTP form
 document.querySelector('.otpNewBtn').addEventListener("click", () => {
     const dom = document.querySelector('.newOTP');
