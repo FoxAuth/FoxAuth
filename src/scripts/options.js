c5fd821384ee8dfeb8c5b5d1a35a135e99bc9f26
Riajyuu <16912240+Riajyuu@users.noreply.github.com>
2018-10-10 13:19:12 +0800

    checkbox fix draft

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -100,3 +100,24 @@ if (qrresult.length > 0) {
     document.getElementById('newSecretToken').value = scannedotp.searchParams.get('secret')
     document.getElementById('newIssuer').value = scannedotp.searchParams.get('issuer')
 }
+
+//toggle checkbox
+[...document.getElementsByClassName('ulPass')].forEach(ul => {
+    const applyNest = p => {
+      if (!p) return;
+      const parent = p.querySelector('input[type=checkbox]');
+      const cp = p.querySelector('ul');
+      console.log(parent, cp);
+      if (cp) {
+        const children = cp.children;
+        parent.addEventListener('input', () => {
+          [...children].forEach(e => {
+            const i = e.querySelector('input[type=checkbox]');
+            if (i) i.disabled = !parent.checked;
+          });
+        });
+        [...children].forEach(applyNest);
+      }
+    };
+    [...ul.children].forEach(applyNest);
+  });
