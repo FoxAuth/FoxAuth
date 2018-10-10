3d00718626d5ab6b58d3b613bd81c3fba7954b78
RalfZhang <zhangkang103@gmail.com>
2018-10-11 00:34:23 +0800

    feat: handling tokens.html click action and spliting public js

--- src/scripts/tokens.js
+++ src/scripts/tokens.js
@@ -1,34 +1,12 @@
 
-//toggle additional form items
+//delete OTP form
 document.body.addEventListener("click", (e) => {
   const t = e.target;
-  if (!t.classList || !t.classList.contains('moreFormbtn')) {
+  if (!t.classList || !t.classList.contains('deleteOTP')) {
       return;
   }
-  const x = t.parentNode.parentNode.querySelector('.moreFormItem');
-  if (window.getComputedStyle(x).display === "none") {
-      x.style.display = "block";
-  } else {
-      x.style.display = "none";
+  const node = t.parentNode.parentNode.parentNode;
+  if(window.confirm("Delete these account info doesn't disable account 2FA for you. Make sure you won't be locked out before proceed.")){
+    node.parentNode.removeChild(node);
   }
 });
-
-//container tabs
-var div = document.getElementById('containerAssign');
-
-if (browser.contextualIdentities === undefined) {
-    div.setAttribute("disable");
-} else {
-  browser.contextualIdentities.query({})
-    .then((identities) => {
-      if (!identities.length) {
-        div.setAttribute("disable");
-        return;
-      }
-      identities.map(x => {
-        const opt = document.createElement('option')
-        opt.innerHTML = x.name
-        return opt
-      }).forEach(x => document.querySelector('select').appendChild(x))
-  });
-}
