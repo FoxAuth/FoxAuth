bfefa4b9fc193db38b666063da8b88dc21f330e3
huihuimoe <0w0@loli.pet>
2018-10-10 17:57:39 +0800

    fix QRCode scan

--- src/scripts/scanQR.js
+++ src/scripts/scanQR.js
@@ -6,18 +6,8 @@ document.getElementById('scanQRPopup').addEventListener('click', e => {
   browser.tabs.captureTab().then(dataURL => {
     QrScanner.scanImage(dataURL)
       .then(result => {
-        // @TODO parse OTP here
-        // @example OTP result
-        // otpauth://totp/testuser@security-totp.appspot.com?secret=DTVXIP2E4QREBVASTAK3M3JWPBUVODJP
-
-        
-        // open new tab to add OTP
-        const params = new URLSearchParams()
-        params.set('key', 'JBSWY3DPEHPK3PXR')
-        params.set('username', 'test')
-        params.set('url', 'example.com/7')
         browser.tabs.create({
-          url: `otpinfo.html?${params.toString()}`
+          url: `otpinfo.html?${encodeURIComponent(result)}`
         })
       })
       .catch(error => {
