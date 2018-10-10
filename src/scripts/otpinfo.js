a4dd213a04bb73a503fbbd0c3d181b7a8ea30682
huihuimoe <0w0@loli.pet>
2018-10-10 17:41:04 +0800

    combine tokens and otpinfo scripts

--- src/scripts/otpinfo.js
+++ src/scripts/otpinfo.js
@@ -1,17 +1,3 @@
-//toggle additional form items
-document.body.addEventListener("click", (e) => {
-    const t = e.target;
-    if (!t.classList || !t.classList.contains('moreFormbtn')) {
-        return;
-    }
-    const x = t.parentNode.querySelector('.moreFormItem');
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-});
-
 //init OTP form stack object
 const otpFormStack = [];
 const otpBasicForm = document.querySelector('.newOTP').cloneNode(true);
@@ -58,28 +44,6 @@ document.querySelector(".eye").addEventListener("click", function () {
         hide();
     }
 }, false);
-
-
-//container tabs
-var div = document.getElementById('containerAssign');
-
-if (browser.contextualIdentities === undefined) {
-    div.setAttribute("disable");
-} else {
-  browser.contextualIdentities.query({})
-    .then((identities) => {
-      if (!identities.length) {
-        div.setAttribute("disable");
-        return;
-      }
-      identities.map(x => {
-        const opt = document.createElement('option')
-        opt.innerHTML = x.name
-        return opt
-      }).forEach(x => document.querySelector('select').appendChild(x))
-  });
-}
-
 
 //Get QrScan Result
 var qrresult = decodeURIComponent(window.location.search.substring(1))
