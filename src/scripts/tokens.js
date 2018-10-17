bc8a9459d8fd82274e1f1d852f7b60684631f960
Bruce Huang <oursyida@gmail.com>
2018-10-17 19:54:38 +0800

    feat: init and render account info

--- src/scripts/tokens.js
+++ src/scripts/tokens.js
@@ -5,6 +5,9 @@
   const warningMsgCloseBtn = warningMsg.querySelector('.warningMsgCloseBtn');
   const warningMsgBtn = warningMsg.querySelector('.warningMsgBtn');
   let confirmFun = function () { };
+  let cachedAccountInfos = [];
+  const formBox = document.getElementById('otpFormBox');
+  const defaultAccountInfoForm = formBox.children[0];
 
   warningMsgCloseBtn.addEventListener('click', function () {
     warningMsgBtn.removeEventListener('click', confirmFun);
@@ -30,6 +33,61 @@
     warningMsgBtn.addEventListener('click', confirmFun);
 
   });
+
+  initInfos();
+  async function initInfos() {
+    const infos = await getInfosFromLocal();
+    if (infos.length > 0) {
+      cachedAccountInfos = infos;
+    } else {
+      // give some samples
+      cachedAccountInfos = [getRefreshAccountInfo()];
+      saveInfosToLocal(cachedAccountInfos);
+    }
+    const newNode = defaultAccountInfoForm.cloneNode(true);
+    updateInfoForm(newNode, cachedAccountInfos[0]);
+    formBox.replaceChild(newNode, defaultAccountInfoForm);
+    formBox.appendChild(generateInfosFragment(cachedAccountInfos.slice(1)));
+  }
+  async function getInfosFromLocal() {
+    const obj = await browser.storage.local.get('accountInfos');
+    const { accountInfos } = obj;
+    return Array.isArray(accountInfos) ? accountInfos : [];
+  }
+  function saveInfosToLocal(infos) {
+    browser.storage.local.set({
+      accountInfos: infos
+    });
+  }
+  function getRefreshAccountInfo() {
+    return {
+      containerAssign: 'none',
+      localIssuer: '',
+      localAccountName: '',
+      localSecretToken: '',
+      localRecovery: '',
+      localOTPType: 'Time based',
+      localOTPAlgorithm: 'SHA-1',
+      localOTPPeriod: '30',
+      localOTPDigits: '6'
+    };
+  }
+  function updateInfoForm(form, info) {
+    Object.keys(info).forEach((key) => {
+      const element = form.querySelector(`[name=${key}]`);
+      if (element) {
+        element.value = info[key];
+      }
+    });
+  }
+  function generateInfosFragment(accountInfos) {
+    return accountInfos.reduce((fragment, info) => {
+      const form = defaultAccountInfoForm.cloneNode(true);
+      updateInfoForm(form, info);
+      fragment.appendChild(form);
+      return fragment;
+    }, document.createDocumentFragment());
+  }
 
 })();
 
