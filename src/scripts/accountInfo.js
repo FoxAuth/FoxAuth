65cd8a8718c0490bd50f4266ae7b559a278368d0
Bruce Huang <oursyida@gmail.com>
2018-10-22 19:58:07 +0800

    feat: encrypt and decrypt data

--- src/scripts/accountInfo.js
+++ src/scripts/accountInfo.js
@@ -1,31 +1,66 @@
-// TODO: sessionStorage and decrypt
 async function getAccountInfos() {
     let accountInfos = [];
     accountInfos = await getInfosFromLocal();
-    return Array.isArray(accountInfos) ? accountInfos : [];
+    accountInfos = Array.isArray(accountInfos) ? accountInfos : [];
+    const storageArea = await getPasswordStorageArea();
+    const passwordInfo = await getPasswordInfo(storageArea);
+    if (passwordInfo.isEncrypted && passwordInfo.password) {
+        accountInfos = await decryptAccountInfos(accountInfos, passwordInfo.password);
+    }
+    return accountInfos;
 }
-// TODO: sessionStorage and encrypt
-function saveAccountInfos(infos) {
-    saveInfosToLocal(infos);
+
+async function saveAccountInfos(infos) {
+    const storageArea = await getPasswordStorageArea();
+    const passwordInfo = await getPasswordInfo(storageArea);
+    if (passwordInfo.isEncrypted && passwordInfo.password) {
+        infos = await encryptAccountInfos(infos, passwordInfo.password);
+    }   
+    await saveInfosToLocal(infos);
 }
 async function getInfosFromLocal() {
     const obj = await browser.storage.local.get('accountInfos');
-    const { accountInfos } = obj;
+    const {
+        accountInfos
+    } = obj;
     return accountInfos;
 }
+
 function saveInfosToLocal(infos) {
-    browser.storage.local.set({
+    return browser.storage.local.set({
         accountInfos: infos
     });
 }
-// TODO: encrypt account name/secret tokens/recovery
-function encryptAccountInfos() {
-
+// encrypt account name/secret tokens/recovery
+function encryptAccountInfos(infos, password) {
+    return __encryptAndDecrypt(infos, password, 'encrypt');
 }
-// TODO: decrypt account name/secret tokens/recovery
-function decryptAccountInfos() {
-
+// decrypt account name/secret tokens/recovery
+function decryptAccountInfos(infos, password) {
+    return __encryptAndDecrypt(infos, password, 'decrypt');
 }
+async function __encryptAndDecrypt(infos, password, invokeFuncName) {
+    const crypto = new MessageEncryption(password);
+    const promiseArr = infos.reduce((result, info) => {
+        const arr = ['localAccountName', 'localSecretToken', 'localRecovery']
+            .reduce((result, key) => {
+                    result.push(
+                        crypto[invokeFuncName](info[key] || '')
+                        .then(value => info[key] = value)
+                    );
+                    return result;
+                },
+                []
+            );
+        result = result.concat(arr);
+        return result;
+    }, []);
+    await Promise.all(promiseArr);
+    const a = new MessageEncryption('123')
+    a.decrypt(infos[0].localAccountName).then(a => console.log(a))
+    return infos;
+}
+
 // same issuer and containerId
 function isSameAccountInfo(info1, info2) {
     return info1.containerAssign === info2.containerAssign &&
@@ -50,4 +85,48 @@ function getDefaultAccountInfo() {
         localOTPPeriod: '30',
         localOTPDigits: '6'
     };
+}
+async function getPasswordStorageArea() {
+    const data = await browser.storage.local.get({
+        settings: {
+            passwordStorage: 'storage.local'
+        }
+    });
+    if (!data.settings || !data.settings.passwordStorage) {
+        return 'storage.local';
+    } else {
+        return data.settings.passwordStorage;
+    }
+}
+async function getPasswordInfo(storageArea) {
+    function base64Decode(str, encoding = 'utf-8') {
+        var bytes = base64js.toByteArray(str);
+        return new(TextDecoder || TextDecoderLite)(encoding).decode(bytes);
+    }
+    const data = await browser.storage.local.get({
+        isEncrypted: false,
+    });
+    const isEncrypted = data.isEncrypted || false;
+    let password = '';
+    if (storageArea === 'storage.local') {
+        const data = await browser.storage.local.get({
+            encryptPassword: '',
+        });
+        password = data.encryptPassword || '';
+    } else {
+        password = jsonParse(sessionStorage.getItem('encryptPassword')) || '';
+    }
+    password = base64Decode(password);
+    return {
+        isEncrypted,
+        password
+    };
+}
+
+function jsonParse(str) {
+    try {
+        return JSON.parse(str);
+    } catch (error) {
+        return null;
+    }
 }
--- src/scripts/sync.js
+++ src/scripts/sync.js
@@ -20,17 +20,148 @@
 });
 
 //msgDismiss
-document.getElementsByClassName("syncbtn").addEventListener("click", () => {
-  document.getElementsByClassName("genericMsg").removeAttribute("display: none")
-});
+// document.getElementsByClassName("syncbtn").addEventListener("click", () => {
+//   document.getElementsByClassName("genericMsg").removeAttribute("display: none")
+// });
 
 //import handling
-var fileSelect = document.getElementById("operatebtn"),
-  fileElem = document.getElementById("fileElem");
+// var fileSelect = document.getElementById("operatebtn"),
+//   fileElem = document.getElementById("fileElem");
 
-fileSelect.addEventListener("click", function (e) {
-  if (fileElem) {
-    fileElem.click();
+// fileSelect.addEventListener("click", function (e) {
+//   if (fileElem) {
+//     fileElem.click();
+//   }
+//   e.preventDefault();
+// }, false);
+
+const encryptForm = document.getElementById('encryptForm');
+const passwordInput = document.getElementById('encryptPass');
+const reconfirmInput = document.getElementById('reconfirmPass');
+const confirmBtn = document.getElementById('confirmBtn');
+const forgetBtn = document.getElementById('forgetBtn');
+const radioList = document.getElementsByName('rememPass');
+const doResetAccountInfos = lockAsyncFunc(
+  async (nextStorageArea, nextPassword) => {
+    const infos = await getAccountInfos();
+    await browser.storage.local.remove('encryptPassword');
+    sessionStorage.removeItem('encryptPassword');
+    const settingsData = await browser.storage.local.get({
+      settings: {}
+    });
+    const settings = settingsData.settings;
+    await setNextPassword(nextStorageArea, nextPassword);
+    await browser.storage.local.set({
+      settings: {
+        ...settings,
+        passwordStorage: nextStorageArea
+      },
+      isEncrypted: true,
+    });
+    await saveAccountInfos(infos);
+    passwordInput.value = '';
+    reconfirmInput.value = '';
+    setConfirmBtnStatus();
   }
-  e.preventDefault();
-}, false);
+);
+passwordInput.addEventListener('input', () => {
+  setConfirmBtnStatus();
+});
+forgetBtn.addEventListener('click', (event) => {
+  console.log('forget password');
+});
+encryptForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const { value: passwordOne } = passwordInput;
+  const { value: passwordTwo } = reconfirmInput;
+  if (
+    passwordOne.length > 0 &&
+    passwordTwo.length > 0 &&
+    passwordOne === passwordTwo
+  ) {
+    // doResetAccountInfos(getCheckedRadioValue(radioList), passwordOne);
+  }
+});
+init();
+
+async function init() {
+  const data = await browser.storage.local.get({
+    settings: {
+      passwordStorage: 'storage.local'
+    }
+  });
+  const { settings } = data;
+  if (
+    !settings ||
+    !settings.passwordStorage ||
+    settings.passwordStorage === 'storage.local') {
+    checkRadioByValue(radioList, 'storage.local');
+  } else {
+    checkRadioByValue(radioList, 'sessionStorage');
+  }
+  setConfirmBtnStatus();
+}
+function forEach(arrayLike, func) {
+  if (arrayLike && arrayLike.length > 0) {
+    const { length } = arrayLike;
+    for(let i = 0; i < length; i++) {
+      if (func(arrayLike[i], i, arrayLike) === false) {
+        break;
+      }
+    }
+  }
+}
+function findBy(arrayLike, func) {
+  let index = -1;
+  forEach(arrayLike, (item, iter, array) => {
+    if (func(item, iter, array)) {
+      index = iter;
+      return false;
+    }
+  });
+  if (index >= 0) {
+    return arrayLike[index];
+  } else {
+    return undefined;
+  }
+}
+function setConfirmBtnStatus() {
+  if (passwordInput.value.length > 0) {
+    confirmBtn.removeAttribute('disabled');
+  } else {
+    confirmBtn.setAttribute('disabled', 'true');
+  }
+}
+function getCheckedRadioValue(radioList) {
+  const radio = findBy(radioList, (radio) => radio.checked);
+  if (radio) {
+    return radio.value;
+  } else {
+    return undefined;
+  }
+}
+function checkRadioByValue(radioList, value) {
+  forEach(radioList, (radio) => {
+    if (radio.value === value) {
+      radio.checked = true;
+      return false;
+    } else {
+      radio.checked = false;
+    }
+  });
+}
+
+async function setNextPassword(nextStorageArea, nextPassword) {
+  function base64Encode(str, encoding = 'utf-8') {
+    var bytes = new (TextEncoder || TextEncoderLite)(encoding).encode(str);        
+    return base64js.fromByteArray(bytes);
+  }
+  nextPassword = base64Encode(nextPassword || '');
+  if (nextStorageArea === 'storage.local') {
+    await browser.storage.local.set({
+      encryptPassword: nextPassword
+    });
+  } else {
+    sessionStorage.setItem('encryptPassword', JSON.stringify(nextPassword));
+  }
+}
