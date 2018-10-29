cc5bb438af879f93ce976f1455e5f038630c583a
Bruce Huang <oursyida@gmail.com>
2018-10-29 21:29:35 +0800

    fix with base32 bug

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -48,6 +48,7 @@ otpContainer.$mount({ target: document.getElementById('otpContainer'), option: '
 var otpStoreInterval = []
 function addOTP(issuer, containerObj = {}, key, expiry = 30, code_length = 6, option = {}) {
   var totp = new jsOTP.totp(expiry, code_length)
+  key = base32tohex(key)
   var id = otpContainer.otppoint.push(new template_totp({
     $data: {
       i18n_Copy: 'Copy',
@@ -117,6 +118,48 @@ function autoFillButtonInit(){
       }
     )
   });
+}
+
+// see https://tools.ietf.org/html/rfc4648#section-6 and https://github.com/Authenticator-Extension/Authenticator/commit/c6d85480d719a9de5bb7d21808e8172b205cfb63
+function base32tohex(base32) {
+  const base32chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
+  let bits = '';
+  let hex = '';
+  let padding = 0;
+
+  for (let i = 0; i < base32.length; i++) {
+    if (base32.charAt(i) === '=') {
+      bits += '00000';
+      padding++;
+    } else {
+      let val = base32chars.indexOf(base32.charAt(i).toUpperCase());
+      val = val.toString(2).padStart(5, '0');
+      bits += val;
+    }
+  }
+  for (let i = 0; i + 4 <= bits.length; i += 4) {
+    const chunk = bits.substr(i, 4);
+    hex = hex + Number(`0b${chunk}`).toString(16);
+  }
+  switch (padding) {
+    case 0:
+      break;
+    case 6:
+      hex = hex.substr(0, hex.length - 8);
+      break;
+    case 4:
+      hex = hex.substr(0, hex.length - 6);
+      break;
+    case 3:
+      hex = hex.substr(0, hex.length - 4);
+      break;
+    case 1:
+      hex = hex.substr(0, hex.length - 2);
+      break;
+    default:
+      throw new Error('Invalid Base32 string');
+  }
+  return hex;
 }
 
 (async function () {
