cb1dbc416153b04b51e367f7ccbdf11ed8a13aef
Bruce Huang <oursyida@gmail.com>
2018-10-30 22:52:57 +0800

    fix with OTP generation Bug

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -47,13 +47,11 @@ const template_totp = ef.t`
 otpContainer.$mount({ target: document.getElementById('otpContainer'), option: 'replace' })
 var otpStoreInterval = []
 function addOTP(issuer, containerObj = {}, key, expiry = 30, code_length = 6, option = {}) {
-  var totp = new jsOTP.totp(expiry, code_length)
-  key = base32tohex(key)
   var id = otpContainer.otppoint.push(new template_totp({
     $data: {
       i18n_Copy: 'Copy',
       i18n_Edit: 'Edit',
-      OTP: totp.getOtp(key),
+      OTP: KeyUtilities.generate(1, key, code_length, expiry),
       issuer: issuer,
       issuerIcon: serviceIconNames.find(e => issuer.toLowerCase().indexOf(e) >= 0 ) || 'fallback',
       container: containerObj.name,
@@ -67,7 +65,7 @@ function addOTP(issuer, containerObj = {}, key, expiry = 30, code_length = 6, op
     }
   })) - 1
   otpStoreInterval.push(setInterval(function () {
-    otpContainer.otppoint[id].$data.OTP = totp.getOtp(key)
+    otpContainer.otppoint[id].$data.OTP = KeyUtilities.generate(1, key, code_length, expiry)
     otpContainer.otppoint[id].$data.progress = expiry - (Math.round(new Date().getTime() / 1000.0) % expiry)
   }, 500))
 }
@@ -118,48 +116,6 @@ function autoFillButtonInit(){
       }
     )
   });
-}
-
-// see https://tools.ietf.org/html/rfc4648#section-6 and https://github.com/Authenticator-Extension/Authenticator/commit/c6d85480d719a9de5bb7d21808e8172b205cfb63
-function base32tohex(base32) {
-  const base32chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
-  let bits = '';
-  let hex = '';
-  let padding = 0;
-
-  for (let i = 0; i < base32.length; i++) {
-    if (base32.charAt(i) === '=') {
-      bits += '00000';
-      padding++;
-    } else {
-      let val = base32chars.indexOf(base32.charAt(i).toUpperCase());
-      val = val.toString(2).padStart(5, '0');
-      bits += val;
-    }
-  }
-  for (let i = 0; i + 4 <= bits.length; i += 4) {
-    const chunk = bits.substr(i, 4);
-    hex = hex + Number(`0b${chunk}`).toString(16);
-  }
-  switch (padding) {
-    case 0:
-      break;
-    case 6:
-      hex = hex.substr(0, hex.length - 8);
-      break;
-    case 4:
-      hex = hex.substr(0, hex.length - 6);
-      break;
-    case 3:
-      hex = hex.substr(0, hex.length - 4);
-      break;
-    case 1:
-      hex = hex.substr(0, hex.length - 2);
-      break;
-    default:
-      throw new Error('Invalid Base32 string');
-  }
-  return hex;
 }
 
 (async function () {
