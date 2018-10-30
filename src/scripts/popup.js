6676e873dc070d2877d42379c2d16525a41f8179
Riajyuu <16912240+Riajyuu@users.noreply.github.com>
2018-10-30 22:15:44 +0800

    minor typo

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -179,7 +179,7 @@ function base32tohex(base32) {
     if(!accountInfos || !accountInfos.length) {
       const emptyDom = document.createElement('div');
       emptyDom.setAttribute('class', 'popup-empty');
-      emptyDom.innerHTML='<img src="../icons/options/starfleet.svg"/><div>Live long and prosperous</div>';
+      emptyDom.innerHTML='<img src="../icons/options/starfleet.svg"/><div>Live long and prosper</div>';
       document.body.appendChild(emptyDom);
       return;
     }
