1d13097cd345521e79f0e2d2c09acf0e3fe26e0b
Riajyuu <16912240+Riajyuu@users.noreply.github.com>
2018-11-01 15:22:54 +0800

    hotfix for QR

--- src/scripts/doScanQR.js
+++ src/scripts/doScanQR.js
@@ -1,11 +1,20 @@
 async function doScanQR() {
+    function getImage(url) {
+        return new Promise((resolve, reject) => {
+            const image = new Image()
+            image.onload = () => resolve(image)
+            image.onerror = () => reject('Can\'t load scan result')
+            image.src = url
+        });
+    }
     const activeTabs = await browser.tabs.query({
         active: true
     })
     if (activeTabs.length !== 1) return
     const activeTab = activeTabs[0]
     const dataURL = await browser.tabs.captureTab(activeTab.id)
-    const result = await QrScanner.scanImage(dataURL)
+    const image = await getImage(dataURL)
+    const result = await QrScanner.scanImage(image)
     // validate and parse URL
     const otpInfo = urlOtpauth.parse(result)
     otpInfo.container = activeTab.cookieStoreId === 'firefox-default' ? '' : activeTab.cookieStoreId
