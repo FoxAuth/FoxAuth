c06d5aad29dc879faacd694beec1d7b32ea5f2cc
爆発は芸術です <16912240+Riajyuu@users.noreply.github.com>
2019-04-08 17:42:22 +0800

    set browseraction title as well

--- src/scripts/background/background.js
+++ src/scripts/background/background.js
@@ -110,6 +110,7 @@ async function setBadgeAsLength() {
     var {accountInfos: arr} = await browser.storage.local.get("accountInfos");
     var textString = arr.length.toString();
     browser.browserAction.setBadgeText({text: textString});
+    browser.browserAction.setTitle({title: textString + " account(s) added."});
 }
 
 browser.browserAction.setBadgeBackgroundColor({color: "#0ff036"});
