1fc245f99d63f9ae62328ef6199690d5b4c22465
Bruce Huang <oursyida@gmail.com>
2019-05-14 19:49:38 +0800

    fix render i18n text

--- src/scripts/i18n.js
+++ src/scripts/i18n.js
@@ -23,17 +23,12 @@ function render(context) {
   const i18ns = context.querySelectorAll('[data-i18n]')
   for (const elem of i18ns) {
     const text = document.createTextNode(getMessage(elem.getAttribute('data-i18n')))
-    const span = document.createElement('span')
-    span.appendChild(text)
-    elem.appendChild(span)
+    elem.appendChild(text)
   }
 
   const htmls = context.querySelectorAll('[data-i18n-html]')
   for (const elem of htmls) {
-    const message = elem.getAttribute('data-i18n-html')
-    const span = document.createElement('span')
-    span.innerHTML = getMessage(message)
-    elem.appendChild(span)
+    elem.innerHTML = getMessage(elem.getAttribute('data-i18n-html'))
   }
 
   renderAttribute(context, 'data-i18n-placeholder', 'placeholder')
