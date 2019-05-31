c854a5c1cee76dfbf91175a5f311d2091e310dc0
爆発は芸術です <16912240+Riajyuu@users.noreply.github.com>
2019-05-31 14:05:26 +0800

    remove code no longer in use

--- src/scripts/sync.js
+++ src/scripts/sync.js
@@ -14,26 +14,6 @@ import * as i18n from './i18n.js';
 
 i18n.render();
 
-//toggle checkbox
-[...document.getElementsByClassName('ulPass')].forEach(ul => {
-  const applyNest = p => {
-    if (!p) return;
-    const parent = p.querySelector('input[type=checkbox]');
-    const cp = p.querySelector('ul');
-    if (cp) {
-      const children = cp.children;
-      parent.addEventListener('input', () => {
-        [...children].forEach(e => {
-          const i = e.querySelector('input[type=checkbox]');
-          if (i) i.disabled = !parent.checked;
-        });
-      });
-      [...children].forEach(applyNest);
-    }
-  };
-  [...ul.children].forEach(applyNest);
-});
-
 //msgDismiss
 // document.getElementsByClassName("syncbtn").addEventListener("click", () => {
 //   document.getElementsByClassName("genericMsg").removeAttribute("display: none")
